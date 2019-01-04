2c5d006411f0effddf01ee96b5662d659859fedb
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-01-04 17:18:53 +0100

    add an option ti fix stuff

--- check.sh
+++ check.sh
@@ -10,6 +10,7 @@ test -z "$BACKUP_ROOT"    && exit 2
 test -z "$BACKUP_CURRENT" && BACKUP_CURRENT=$BACKUP_ROOT/current
 test -z "$BACKUP_FLOCK"   && BACKUP_FLOCK=$BACKUP_ROOT/lock
 test -z "$BACKUP_MAIN"    && BACKUP_MAIN=$BACKUP_ROOT/data
+test -z "$BACKUP_LIST"    && BACKUP_LIST=$BACKUP_ROOT/files.txt
 test -z "$BACKUP_DB"      && BACKUP_DB=$BACKUP_ROOT/backup.db
 
 SQLITE="sqlite3 $BACKUP_DB"
@@ -18,6 +19,7 @@ SQLITE="sqlite3 $BACKUP_DB"
 exec 200>"$BACKUP_FLOCK"
 flock -n 200 || exit 200
 
+test "$1" = '--fix' && FIX_MISSING=1
 test "$1" = '--delete' && DELETE_MISSING=1
 
 echo "Checking DB => FS"
@@ -59,8 +61,11 @@ find "$BACKUP_CURRENT" \( -type f -o -type l \) -printf '%P\n' | while read full
 		ELSE '$clean_fullname'
 		END;"
 done | $SQLITE | fgrep -v -x 1 | (
-	echo "Orphan files in $BACKUP_CURRENT:" >&2
-	if test -n "$DELETE_MISSING"; then
+	if test -n "$FIX_MISSING"; then
+		cd "$BACKUP_CURRENT"
+		tee /dev/stderr | fgrep -v -f- "$BACKUP_LIST" >"$BACKUP_LIST.new"
+		mv "$BACKUP_LIST.new" "$BACKUP_LIST"
+	elif test -n "$DELETE_MISSING"; then
 		cd "$BACKUP_CURRENT"
 		tee /dev/stderr | xargs -d '\n' rm -f
 	else
@@ -109,6 +114,8 @@ echo "Checking current FS => old FS"
 find "$BACKUP_CURRENT" \( -type f -o -type l \) -printf '%i %P\n' | while read inode fullname; do
 	ls -if "$BACKUP_MAIN/$fullname" 2>/dev/null | grep -q "^$inode " || echo "$fullname"
 done | (
+	# TODO: if it's the same file we've already "fixed" on "current FS => DB"
+	# step, we don't need to remove it now
 	if test -n "$DELETE_MISSING"; then
 		cd "$BACKUP_CURRENT"
 		tee /dev/stderr | xargs -d '\n' rm -f
@@ -132,6 +139,7 @@ $SQLITE "SELECT a.dirname, a.filename, a.rowid, b.rowid, a.created, a.deleted, b
 	AND a.filename = b.filename
 	AND a.created < b.deleted
 	AND b.created < a.deleted;"
+	# TODO: fix?
 
 
 echo "Checking duplicates in DB"
@@ -149,6 +157,7 @@ $SQLITE "SELECT a.dirname, a.filename, a.rowid, b.rowid, a.created, a.deleted, b
 	AND a.filename = b.filename
 	AND ( a.created = b.created
 	   OR a.deleted = b.deleted);"
+	# TODO: delete
 
 
 echo "Checking that created < deleted in DB"
@@ -157,26 +166,29 @@ $SQLITE "SELECT *
 	FROM history
 	WHERE created >= deleted;"
 
-echo "Checking freq in DB"
 
-$SQLITE "SELECT *
-	FROM history
-	WHERE freq != 0 AND
-	freq != CASE
-		WHEN substr(created, 1, 7) != substr(deleted, 1, 7) THEN 1 -- different month
-		WHEN strftime('%Y %W', created) != strftime('%Y %W', deleted) THEN 5 -- different week
-		WHEN substr(created, 1, 10) != substr(deleted, 1, 10) THEN 30 -- different day
-		WHEN substr(created, 1, 13) != substr(deleted, 1, 13) THEN 720 -- different hour
-		ELSE 8640
-	END;"
+if test -n "$FIX_MISSING"; then
+	echo "Fixing freq in DB"
+	$SQLITE "UPDATE history
+		SET freq = CASE
+			WHEN substr(created, 1, 7) != substr(deleted, 1, 7) THEN 1 -- different month
+			WHEN strftime('%Y %W', created) != strftime('%Y %W', deleted) THEN 5 -- different week
+			WHEN substr(created, 1, 10) != substr(deleted, 1, 10) THEN 30 -- different day
+			WHEN substr(created, 1, 13) != substr(deleted, 1, 13) THEN 720 -- different hour
+			ELSE 8640
+		END
+		WHERE freq != 0;"
+else
+	echo "Checking freq in DB"
+	$SQLITE "SELECT *
+		FROM history
+		WHERE freq != 0 AND
+		freq != CASE
+			WHEN substr(created, 1, 7) != substr(deleted, 1, 7) THEN 1 -- different month
+			WHEN strftime('%Y %W', created) != strftime('%Y %W', deleted) THEN 5 -- different week
+			WHEN substr(created, 1, 10) != substr(deleted, 1, 10) THEN 30 -- different day
+			WHEN substr(created, 1, 13) != substr(deleted, 1, 13) THEN 720 -- different hour
+			ELSE 8640
+		END;"
 
-# $SQLITE "UPDATE history
-# 	SET freq = CASE
-# 		WHEN substr(created, 1, 7) != substr(deleted, 1, 7) THEN 1 -- different month
-# 		WHEN strftime('%Y %W', created) != strftime('%Y %W', deleted) THEN 5 -- different week
-# 		WHEN substr(created, 1, 10) != substr(deleted, 1, 10) THEN 30 -- different day
-# 		WHEN substr(created, 1, 13) != substr(deleted, 1, 13) THEN 720 -- different hour
-# 		ELSE 8640
-# 	END
-# 	WHERE freq != 0;"
-
+fi
