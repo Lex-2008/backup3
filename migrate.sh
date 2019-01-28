b40d31b52062c665f8ce9078e4f1eb7a05f8e7bb
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-01-28 15:11:17 +0100

    Rework migrate.sh to work in two steps
    Also allow dashes instead of colons in dirnames

--- migrate.sh
+++ migrate.sh
@@ -4,8 +4,18 @@
 #
 # "old style" expected to be like {hourly,daily,monthly,etc}/$(date +"%F_%T")
 #
-# Call it like this:
-# $ conv.sh /backups/homes homes
+# First, create list of dirs to process:
+# $ migrate.sh --files /backups/homes/*/* >files.txt
+#
+# Then, call it like this to process the generated list:
+# $ migrate.sh files.txt homes | tee log.log
+
+
+if test "$1" = "--files"; then
+	shift
+	ls -d "$@" | sed -r 's_(.*)/(.*)_\1 \2_' | sort -k 2 | uniq -f 1 | sed 's_(.*) (.*)_\1/\2_'
+	exit
+fi
 
 test -z "$BACKUP_ROOT"    && exit 2
 
@@ -14,20 +24,18 @@ test -z "$BACKUP_CURRENT" && BACKUP_CURRENT=$BACKUP_ROOT/current
 BACKUP_DB_BAK=no
 export BACKUP_DB_BAK
 
-SRC="$1"
+files="$1"
 DST="$2"
 
-test -e "$BACKUP_CURRENT/$DST" && exit 3
-
-(cd "$SRC"; ls -d */*/) | sed 's_/_ _' | sort -k 2 | uniq -f 1 | sed 's_ _/_' | (
-	while read dir; do
-		mv "$SRC/$dir" "$BACKUP_CURRENT/$DST"
-		time="${dir#*/}"
-		time="${time%/}"
-		export BACKUP_TIME="$(date -d "$(echo "$time" | sed 's/_/ /')" +"%F %H:%M")"
-		echo "processing [$BACKUP_TIME] from [$SRC/$dir]..."
-		bash ~/backup3/backup.sh
-		mv "$BACKUP_CURRENT/$DST" "$SRC/$dir"
-	done
-	echo remember to move last directory back to "$BACKUP_CURRENT/$DST"
-)
+while read dir; do
+	test -f "$files.stop"
+	echo "no $files.stop file, moving on!"
+	time="${dir%/}"
+	time="${time##*/}"
+	export BACKUP_TIME="$(date -d "$(echo "$time" | sed -r 's/_/ /;s/(.*)-(.*)-(.*)/\1:\2:\3/')" +"%F %H:%M")"
+	echo "processing [$BACKUP_TIME] from [$dir]..."
+	mv "$dir" "$BACKUP_CURRENT/$DST"
+	bash ~/backup3/backup.sh
+	mv "$BACKUP_CURRENT/$DST" "$dir"
+done <"$files"
+echo remember to move last directory back to "$BACKUP_CURRENT/$DST"
