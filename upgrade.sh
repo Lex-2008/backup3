11212fbb6115d401f38f344c231fa44e48eef6c6
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-09-22 16:17:52 +0200

    Add comments to upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -12,10 +12,12 @@ test -z "$BACKUP_FIND_FILTER" # this is fine
 
 SQLITE="sqlite3 $BACKUP_DB"
 
+echo 'Step 1: updating deleted timestamps'
 $SQLITE "UPDATE history
 	SET deleted = '$BACKUP_TIME_NOW'
 	WHERE freq = 0;"
 
+echo 'Step 2: renaming old files'
 $SQLITE "SELECT dirname,
 		filename,
 		created,
@@ -24,7 +26,8 @@ $SQLITE "SELECT dirname,
 	WHERE freq != 0;" | while IFS='|' read dirname filename created deleted; do
 		mv "$BACKUP_MAIN/$dirname/$filename/$created" "$BACKUP_MAIN/$dirname/$filename/$created$BACKUP_TIME_SEP$deleted"
 	done
-	
+
+echo 'Step 3: renaming existing files'
 $SQLITE "SELECT dirname,
 		filename,
 		created
@@ -33,6 +36,7 @@ $SQLITE "SELECT dirname,
 		mv "$BACKUP_MAIN/$dirname/$filename/$created" "$BACKUP_MAIN/$dirname/$filename/$created$BACKUP_TIME_SEP$BACKUP_TIME_NOW"
 	done
 
-
+echo 'Step 4: updating list file'
+# delete file sizes and convert from newline-separated file to zero-separated one
 sed 's/ [0-9]* / /' "$BACKUP_LIST" | tr '\n' '\0' | LC_ALL=POSIX sort -z >"$BACKUP_LIST".new
 mv "$BACKUP_LIST".new "$BACKUP_LIST"
