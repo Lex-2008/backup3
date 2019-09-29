0154ad2f88d4c9ea756e15d3140906930f7bb17f
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-09-29 17:30:46 +0200

    Simplify upgrade process

--- upgrade.sh
+++ upgrade.sh
@@ -3,6 +3,7 @@
 test -z "$BACKUP_ROOT"    && exit 2
 
 test -z "$BACKUP_MAIN"    && BACKUP_MAIN=$BACKUP_ROOT/data
+test -z "$BACKUP_OLD"     && BACKUP_OLD=$BACKUP_ROOT/old
 test -z "$BACKUP_DB"      && BACKUP_DB=$BACKUP_ROOT/backup.db
 test -z "$BACKUP_DB_BAK"  && BACKUP_DB_BAK=backup.db
 test -z "$BACKUP_TIME_SEP" && BACKUP_TIME_SEP="~"
@@ -13,47 +14,40 @@ test -z "$BACKUP_FIND_FILTER" # this is fine
 
 SQLITE="sqlite3 $BACKUP_DB"
 
-echo 'Step 0: backing up'
-bak="$BACKUP_ROOT/pre-migrate"
-mkdir "$bak"
-cp -al "$BACKUP_MAIN" "$bak"
-cp -l "$BACKUP_DB" "$bak"
-cp -l "$BACKUP_LIST" "$bak"
+if test -d "$BACKUP_MAIN"; then
+	echo "$BACKUP_MAIN exists, aborting."
+
+if test -d "$BACKUP_OLD"; then
+	echo "$BACKUP_OLD exists, skipping backing up."
+else
+	echo 'Step 0: backing up'
+	mv "$BACKUP_MAIN" "$BACKUP_OLD"
+	cp "$BACKUP_DB" "$BACKUP_DB.bak"
+	cp "$BACKUP_LIST" "$BACKUP_LIST.bak"
+fi
 
 echo 'Step 1: updating deleted timestamps'
 $SQLITE "UPDATE history
 	SET deleted = '$BACKUP_TIME_NOW'
 	WHERE freq = 0;"
 
-echo 'Step 2: renaming old files'
+echo 'Step 2: renaming files'
 $SQLITE "SELECT dirname,
 		filename,
 		created,
 		deleted
 	FROM history
-	WHERE freq != 0;" | while IFS='|' read dirname filename created deleted; do
-		mv "$BACKUP_MAIN/$dirname/$filename/$created" "$BACKUP_MAIN/$dirname/$filename/$created$BACKUP_TIME_SEP$deleted"
+	WHERE dirname!=''
+	   OR filename!='$BACKUP_DB_BAK';" | while IFS='|' read dirname filename created deleted; do
+		mkdir -p "$BACKUP_MAIN/$dirname/$filename/$created$BACKUP_TIME_SEP$deleted"
+		cp -l "$BACKUP_OLD/$dirname/$filename/$created" "$BACKUP_MAIN/$dirname/$filename/$created$BACKUP_TIME_SEP$deleted"
 	done
 
-echo 'Step 3: renaming existing files'
-$SQLITE "SELECT dirname,
-		filename,
-		created
-	FROM history
-	WHERE freq = 0;" | while IFS='|' read dirname filename created deleted; do
-		mv "$BACKUP_MAIN/$dirname/$filename/$created" "$BACKUP_MAIN/$dirname/$filename/$created$BACKUP_TIME_SEP$BACKUP_TIME_NOW"
-	done
-
-echo 'Step 4: updating list file'
-# delete file sizes and convert from newline-separated file to zero-separated one
-sed -i 's/ [0-9]* / /' "$BACKUP_LIST"
+echo 'Step 3: updating list file'
+# delete backup.db entry and file sizes
+sed -i '/^[0-9]* backup.db/d;s/ [0-9]* / /' "$BACKUP_LIST"
 
 if test "$BACKUP_DB_BAK" != "no"; then
-	echo 'Step 5.1: deleting backup.db backups from filesystem'
-	rm -rf "$BACKUP_MAIN/$BACKUP_DB_BAK"
+	echo 'Step 4: deleting backup.db backups from filesystem'
 	rm -f "$BACKUP_CURRENT/$BACKUP_DB_BAK"
-	echo 'Step 5.2: deleting backup.db backups from database'
-	sqlite3 $BACKUP_DB "DELETE FROM history WHERE dirname='' AND filename='$BACKUP_DB_BAK';"
-	echo 'Step 5.2: deleting backup.db from list file'
-	sed -i '/^[0-9]* backup.db/d' "$BACKUP_LIST"
 fi
